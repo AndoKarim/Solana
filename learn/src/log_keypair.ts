050f527c7b28e6a17ce03fda6b1b775b78c385e4
yvanat <45511469+yvanat@users.noreply.github.com>
2023-12-11 01:18:56 +0100

    Delete learn directory

--- learn/src/log_keypair.ts
+++
@@ -1,6 +0,0 @@
-import { Keypair } from "@solana/web3.js";
-const fs = require("fs")
-const secret = JSON.parse(fs.readFileSync("AaaipcFKbS8LJkR9t7kHwqJRL5Q8gdQe5nYxd6mMucbf.json").toString()) as number[]
-const secretKey = Uint8Array.from(secret)
-const payer = Keypair.fromSecretKey(secretKey)
-console.log(payer.publicKey.toBase58())
